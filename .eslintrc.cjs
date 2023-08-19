bb9ada7a9bd6cdc9730a7ffd9b762cc47569f49b
Dimitrios Chatziioannou <dimitrios@kmatech.co>
2023-08-19 16:13:03 +0100

    eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,9 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
