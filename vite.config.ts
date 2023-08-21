653cefee11fc7b256ce98be062cd4583b9392854
Dimitrios Chatziioannou <dimitrios@kmatech.co>
2023-08-21 09:17:55 +0100

    localhost etc

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
+import dns from 'dns';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
+
+dns.setDefaultResultOrder('verbatim');
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +12,8 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    strictPort: true,
+    host: 'localhost',
+    open: '/',
     proxy: {
       '/api': {
         target: 'http://localhost:3008',
